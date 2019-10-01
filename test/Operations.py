1afd1c1307e80a2ad22b1835105b1510a2a46407
Manuel Goncalves <man281192@gmail.com>
2019-10-01 18:35:13 -0300

    Operations in python

---
+++ test/Operations.py
@@ -0,0 +1,12 @@
+print(2 + 2) #Suma
+print(5 - 2) #Resta
+print(3 * 4) #Multiplicacion
+print(5 / 4) #Division entera
+print(5.0 / 4.0) #Division decimal
+print(9 % 2) #Modulo de la division
+print(10 % 2) #Modulo de la division
+print(3 ** 2) #Potencia
+print(3 > 5) #Mayor que
+print(5 < 10) #Menor que
+print(10 == 10) #Igual
+print(10 == 9) #Igual
