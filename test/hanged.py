9b87ab0d59fdf2274014e23864e9cf95bfa3a0b1
Manuel Goncalves <man281192@gmail.com>
2019-10-20 11:31:15 -0300

    Finish hanged game logic with win and lose message

--- test/hanged.py
+++ test/hanged.py
@@ -17,29 +17,31 @@ IMAGES = [
         |     
         |
      ___|___''',
-    '''        _______
-           |      |
-           |     (_)
-           |     /|
-           |      
-           |     
-           |
-        ___|___''',
-    '''        _______
-           |      |
-           |     (_)
-           |     /|\
-           |      
-           |     
-           |
-        ___|___''', '''        _______
+     '''        _______
+        |      |
+        |     (_)
+        |     /|
+        |     
+        |     
+        |
+     ___|___''',
+     '''        _______
+        |      |
+        |     (_)
+        |     /|\
+        |     
+        |     
+        |
+     ___|___''',
+     '''        _______
         |      |
         |     (_)
         |     /|\
         |      |
-        |     / 
+        |     /
         |
-     ___|___''', '''        _______
+     ___|___''',
+     '''        _______
         |      |
         |     (_)
         |     /|\
@@ -62,7 +64,8 @@ def random_word():
     index = random.randint(0, len(WORDS) - 1)
     return WORDS[index]
 
-def display_board(hidden_word,tries):
+
+def display_board(hidden_word, tries):
     print(IMAGES[tries])
     print(hidden_word)
     print('---- * -----' * 3)
@@ -70,14 +73,29 @@ def display_board(hidden_word,tries):
 
 def run():
     word = random_word()
-    hidden_word = ['-'] * len(WORDS)
+    hidden_word = ['-'] * len(word)
     tries = 0
     while(True):
         display_board(hidden_word,tries)
         user_word = raw_input('escoge una letra:')
-        if(word)
-        break
+        letter_indexes = []
+        for index in range(len(word)):
+            if word[index] == user_word:
+                letter_indexes.append(index)
+        if letter_indexes == []:
+            tries += 1
+        else:
+            for index in letter_indexes:
+                hidden_word[index] = user_word
 
+        if tries == len(IMAGES):
+            print('Has perdido!')
+            print('La palabra correcta era {}'.format(word))
+            break
+        elif not hidden_word.__contains__('-'):
+            print('Has ganado!')
+            print('Felicitaciones! la palabra es {}'.format(word))
+            break
 
 if __name__ == '__main__':
     print('A H O R C A D O')
